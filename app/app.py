35b6dacdef4f97ed6afa6d71d097b0f72da2c056
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2023-06-27 23:47:44 -0300

    teste

--- app/app.py
+++ app/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, Flask app!"
 
+@app.route('/inter/')
+def hehe():
+    return 'nacional'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5099)
 
